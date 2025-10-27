446ad0b86b2412de2e993d509979ad61ccde12c2
sotois <kofiagyapong837@gmail.com>
2025-10-27 12:22:55 -0400

    Add number guessing game with random magic number

--- version-1.py
+++ version-1.py
@@ -1,1 +1,11 @@
+import random as rd
 
+magic_number = rd.randint(1, 10)
+# print(magic_number)
+
+guess = int(input("Guess a number between 1 and 10"))
+
+if guess == magic_number:
+  print("You win!")
+else:
+  print("You lose!")
